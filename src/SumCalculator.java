d0014622342804528656511c35a4943ecf0ad63e
adc40 <abramchestnut@gmail.com>
2015-06-13 16:34:32 -0500

    Next version

--- src/SumCalculator.java
+++ src/SumCalculator.java
@@ -3,7 +3,7 @@
  */
 public class SumCalculator {
     public static void main(String[] args) {
-        int sum = 1;
+        int sum = 0;
         for (int i = 0; i < args.length; i++) {
             sum += Integer.parseInt(args[i]);
         }
